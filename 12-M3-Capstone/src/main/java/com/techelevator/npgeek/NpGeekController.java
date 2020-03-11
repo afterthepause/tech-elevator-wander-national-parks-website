540d4e70f2a9e5b1a61d6df6e07e13f3969d485d
Josef Widdifield <joewidd94@gmail.com>
2020-03-11 13:04:40 -0400

    added path to home page

--- 12-M3-Capstone/src/main/java/com/techelevator/npgeek/NpGeekController.java
+++ 12-M3-Capstone/src/main/java/com/techelevator/npgeek/NpGeekController.java
@@ -1,5 +1,18 @@
 package com.techelevator.npgeek;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class NpGeekController {
 
+	
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public String showHomePage() {
+		return "homePage";
+	}
+	
+	
+	
 }
