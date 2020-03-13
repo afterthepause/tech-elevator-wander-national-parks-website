cf9a7a8e0a215067bd33b25d50a9dd80c3b3788f
Josef Widdifield <joewidd94@gmail.com>
2020-03-13 12:55:13 -0400

    pushing my work for you dude

--- 12-M3-Capstone/src/main/webapp/WEB-INF/jsp/survey.jsp
+++ 12-M3-Capstone/src/main/webapp/WEB-INF/jsp/survey.jsp
@@ -10,24 +10,38 @@
 <form:form action="${surveyURL}" method="POST" modelAttribute="newSurvey">
 
 <label for="parkCode">Choose a Park:</label>
+	<br/>
 <form:select path="parkCode">
 	<c:forEach items="${parks}" var="park">
 	
 		<form:option value="${park.parkCode}"><c:out value="${park.parkName}"/></form:option>
-	
+		
 	</c:forEach>
 </form:select>
-
+		<br/>
+		<br/>
 	<label for="email">Input your email address:</label>
+		<br/>
 	<form:input path="email"/>
+		<br/>
 	<form:errors path="email" cssClass="errors"/>
 		<br/>
 	<label for="state">Input your state of residence:</label>
+		<br/>
 	<form:input path="state"/>
+		<br/>
 	<form:errors path="state" cssClass="errors"/>
 		<br/>
-	<label for="activityLevel">Input how active you are in your life:</label>
-	<form:input path="activityLevel"/>
+	<label for="activityLevel">Select how active you are in your life:</label>
+		<br/>
+	<form:radiobutton value="inactive"  path="activityLevel"/>Inactive
+		<br/>
+	<form:radiobutton value="sedentary"  path="activityLevel"/>Sedentary
+		<br/>
+	<form:radiobutton value="active"  path="activityLevel"/>Active
+		<br/>
+	<form:radiobutton value="extremely active"  path="activityLevel"/>Extremely Active
+		<br/>
 	<form:errors path="activityLevel" cssClass="errors"/>
 		<br/>
 	<input type="submit" value="Let it rip, dude!"/>
--- 12-M3-Capstone/src/test/java/com/techelevator/dao/JdbcParksDaoTest.java
+++ 12-M3-Capstone/src/test/java/com/techelevator/dao/JdbcParksDaoTest.java
@@ -1,18 +1,140 @@
 package com.techelevator.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 import com.techelevator.DAOIntegrationTest;
+import com.techelevator.model.Parks;
+import com.techelevator.model.Weather;
 
 public class JdbcParksDaoTest extends DAOIntegrationTest{
 	
-	@Test
-	public void return_park_by_parkcode() {
+	private ParksDao parksDao;
+	private static SingleConnectionDataSource datasource;
+	private JdbcTemplate jdbcTemplate;
+	private static final String TEST_PARK = "Test Park";
+	
+	@Before
+	public void setup() {
+		datasource = (SingleConnectionDataSource) super.getDataSource();
+		parksDao = new JdbcParksDao(datasource);
+		jdbcTemplate = new JdbcTemplate(datasource);
 		
+		
+		//truncateParkAndWeather();
 	}
 	
+	@Test
+	public void get_park_by_parkcode() {
+		
+		Parks park = new Parks();
+		park.setParkCode("test");
+		park.setParkName("testname");
+		park.setState("teststate");
+		park.setAcreage(100);
+		park.setElevationInFeet(500);
+		park.setMilesOfTrail(20);
+		park.setNumberOfCampsites(20);
+		park.setClimate("testclimate");
+		park.setYearFounded(2000);
+		park.setAnnualVisitorCount(300);
+		park.setInspirationalQuote("testquote");
+		park.setInspirationalQuoteSource("testsource");
+		park.setParkDescription("testdescription");
+		park.setEntryFee(5);
+		park.setNumberOfAnimalSpecies(6);
+		savePark(park);
+		
+		List<Parks> parks = new ArrayList<Parks>();
+		parks.add(parksDao.getByCode(park.getParkCode()));
+		
+		Assert.assertNotNull(parks);
+		Assert.assertEquals(1, parks.size());
+	}
 	
+	@Test
+	public void get_weather_by_parkcode() {
+		Parks park = new Parks();
+		park.setParkCode("test");
+		park.setParkName("testname");
+		park.setState("teststate");
+		park.setAcreage(100);
+		park.setElevationInFeet(500);
+		park.setMilesOfTrail(20);
+		park.setNumberOfCampsites(20);
+		park.setClimate("testclimate");
+		park.setYearFounded(2000);
+		park.setAnnualVisitorCount(300);
+		park.setInspirationalQuote("testquote");
+		park.setInspirationalQuoteSource("testsource");
+		park.setParkDescription("testdescription");
+		park.setEntryFee(5);
+		park.setNumberOfAnimalSpecies(6);
+		savePark(park);
+		
+		Weather weather = new Weather();
+		weather.setParkCode("test");
+		weather.setFiveDayForecastValue(1);
+		weather.setLowFar(10);
+		weather.setHighFar(40);
+		weather.setForecast("snow");
+		saveWeather(weather);
+		
+		List<Weather> weatherList = parksDao.getWeatherByCode(weather.getParkCode());
+		Assert.assertNotNull(weatherList);
+		Assert.assertEquals(1, weatherList.size());
+	}
 	
+	/*
+	 * @Test
+	public void get_all_venues() {
+
+		String sqlInsertVenue = "INSERT INTO venue (id, name, city_id, description) "
+				+ "VALUES (25, ?, 3, 'Something here')";
+		jdbcTemplate.update(sqlInsertVenue, TEST_VENUE);
+
+		boolean hasVenues = false;
+
+		for (Venue venue : dao.getAllVenues()) {
+			String example = venue.getName();
+			if (example.equals("Test Venue")) {
+				hasVenues = true;
+			}
+		}
+
+		Assert.assertTrue(hasVenues);
+	}
+	 */
 	
+	@Test
+	public void get_all_parks() {
+		String sqlInsertPark = String sqlSavePark = "INSERT INTO park(parkcode, parkname, state, acreage, elevationinfeet, milesoftrail, numberofcampsites, climate, yearfounded, annualvisitorcount, inspirationalquote, inspirationalquotesource, parkdescription, entryfee, numberofanimalspecies) "
+				+ "VALUES('test',?,'testState',100,100,100,100,10,'testClimate',2000,100,'testQuote','testSource',?,?,?)";
+	}
+	
+	private Parks savePark(Parks newPark) {
+		
+		String sqlSavePark = "INSERT INTO park(parkcode, parkname, state, acreage, elevationinfeet, milesoftrail, numberofcampsites, climate, yearfounded, annualvisitorcount, inspirationalquote, inspirationalquotesource, parkdescription, entryfee, numberofanimalspecies) "
+				+ "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		jdbcTemplate.update(sqlSavePark, newPark.getParkCode(), newPark.getParkName(), newPark.getState(), newPark.getAcreage(), newPark.getElevationInFeet(), newPark.getMilesOfTrail(),
+				newPark.getNumberOfCampsites(), newPark.getClimate(), newPark.getYearFounded(), newPark.getAnnualVisitorCount(), newPark.getInspirationalQuote(), 
+				newPark.getInspirationalQuoteSource(), newPark.getParkDescription(), newPark.getEntryFee(), newPark.getNumberOfAnimalSpecies() );
+		
+		return newPark;
+	}
+	private Weather saveWeather(Weather newWeather) {
+		
+		String sqlSaveWeather = "INSERT INTO weather(parkcode, fivedayforecastvalue, low, high, forecast) VALUES(?,?,?,?,?)";
+		jdbcTemplate.update(sqlSaveWeather, newWeather.getParkCode(), newWeather.getFiveDayForecastValue(), newWeather.getLowFar(), 
+				newWeather.getHighFar(), newWeather.getForecast());
+		
+		return newWeather;
+	}
 	
 }
